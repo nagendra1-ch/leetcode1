9fa9a1bdeec74ee916c1b403dbaee3dbd9cb7ec1
nagendra1-ch <chnagendra592@gmail.com>
2025-05-16 04:50:35 +0000

    Sync LeetCode submission Runtime - 1 ms (25.84%), Memory - 17.7 MB (100.00%)

---
+++ 2137-final-value-of-variable-after-performing-operations/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        ans=0
+        for i in operations:
+            if i=='++X' or i=='X++':
+                ans=ans+1
+            if i=='--X' or i=='X--':
+                ans=ans-1
+        return ans
+        
