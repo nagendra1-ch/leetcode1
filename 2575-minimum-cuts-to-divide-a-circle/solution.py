0acf9953271d4f26c10a900cec1d27b090d40e1f
nagendra1-ch <chnagendra592@gmail.com>
2025-05-29 14:02:58 +0000

    Sync LeetCode submission Runtime - 3 ms (0.84%), Memory - 18 MB (100.00%)

---
+++ 2575-minimum-cuts-to-divide-a-circle/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfCuts(self, n: int) -> int:
+        if n%2==0:
+            return int(n/2)
+        elif n==1:
+            return 0
+        else:
+            return n
+        
+        
