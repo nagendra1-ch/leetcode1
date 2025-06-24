d5aa2d47aa1f12fd6d7e619a115ab197c2bfd475
nagendra1-ch <chnagendra592@gmail.com>
2025-06-24 10:14:34 +0000

    Sync LeetCode submission Runtime - 7 ms (35.35%), Memory - 18.6 MB (100.00%)

--- 0268-missing-number/solution.py
+++ 0268-missing-number/solution.py
@@ -1,9 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        max=nums[0]
         nums.sort()
-        for i in range(0,max(nums)+2):
-            if i in nums:
+        for i in range(len(nums)):
+            if max<nums[i]:
+                max=nums[i]
+        for i in range(0,max+1):
+            if i == nums[i]:
                 continue
             else:
                 return i
-        
+        return i+1
