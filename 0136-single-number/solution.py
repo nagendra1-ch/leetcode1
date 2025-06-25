44e7acc345cf72a59e3aaf6b5ddce75cf9208d3c
nagendra1-ch <chnagendra592@gmail.com>
2025-06-25 06:13:24 +0000

    Sync LeetCode submission Runtime - 1 ms (70.23%), Memory - 19.5 MB (100.00%)

--- 0136-single-number/solution.py
+++ 0136-single-number/solution.py
@@ -1,8 +1,22 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        frequency=[0]*1000000
-        for i in nums:
-            frequency[i]+=1
-        for i in nums:
-            if frequency[i]==1:
-                return i
+        # frequency=[0]*1000000
+        # for i in nums:
+        #     frequency[i]+=1
+        # for i in nums:
+        #     if frequency[i]==1:
+        # #         return i
+        # dic={}
+        # for i in nums:
+        #     if i in dic:
+        #         dic[i]+=1
+        #     else:
+        #         dic[i]=1
+        # for i in dic:
+        #     if dic[i]==1:
+        #         return i
+        n=len(nums)
+        single=nums[0]
+        for i in range(n-1):
+            single=single^nums[i+1]
+        return single
