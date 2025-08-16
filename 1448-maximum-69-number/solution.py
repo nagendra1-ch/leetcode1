1670b41b30cc4fdd647f432c1aae97f08e7b6a02
nagendra1-ch <chnagendra592@gmail.com>
2025-08-16 08:46:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ 1448-maximum-69-number/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        num=list(str(num))
+        n=len(num)
+        for i in range(n):
+            if num[i]=='6':
+                num[i]='9'
+                break
+        return int(''.join(num))
