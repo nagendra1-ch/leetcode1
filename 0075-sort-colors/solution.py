491e362661b97f96e7a2f9a0e75319ecd484d0de
nagendra1-ch <chnagendra592@gmail.com>
2026-06-02 06:25:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (60.66%)

--- 0075-sort-colors/solution.py
+++ 0075-sort-colors/solution.py
@@ -1,9 +1,14 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        n=len(nums)
-        for i in range(n):
-            for j in range(n):
-                if nums[i]<nums[j]:
-                    nums[i],nums[j]=nums[j],nums[i]
-        
+        d={}
+        for i in range(3):
+            d[i]=0
+        for i in nums:
+                d[i]+=1
+            
+        nums.clear()
+        for k,v in d.items():
+            for i in range(v):
+                nums.append(k)
+
 
