8d9648c8b2e1273fff42c8ab7845f1570d5b46ba
nagendra1-ch <chnagendra592@gmail.com>
2026-06-04 06:09:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (88.28%)

--- 2571-find-the-pivot-integer/solution.py
+++ 2571-find-the-pivot-integer/solution.py
@@ -1,20 +1,7 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        if n==1:
-            return 1
-        sum1=0
-        sum2=0
-        k=0
-        x=n
-        while n>k:
-            sum1=sum(range(1,k+1))
-            sum2=sum(range(k,n+1))
-            if sum1==sum2:
-                return k
-            k=k+1
-            
+        y=n*(n+1)/2
+        x=int(math.sqrt(y))
+        if x*x==int(y):
+            return x
         return -1
-
-            
-
-        
