fb9818a7650daf1fa2a1684f6bbc27ad3ba1771d
nagendra1-ch <chnagendra592@gmail.com>
2026-07-07 06:27:12 +0000

    Sync LeetCode submission Runtime - 3 ms (72.56%), Memory - 19.3 MB (34.01%)

--- 1904-second-largest-digit-in-a-string/solution.py
+++ 1904-second-largest-digit-in-a-string/solution.py
@@ -1,19 +1,17 @@
 class Solution:
     def secondHighest(self, s: str) -> int:
-        s1=''
-        new=['1','2','3','4','5','6','7','8','9','0']
+        second=-1
+        first=None
+        num=None
         for i in s:
-            for j in new:
-                if j==i:
-                    s1+=i+' '
-                    break
-        l=list(map(int,s1.split()))
-        max1=-1
-        secondMax=-1
-        for i in range(len(l)):
-            if max1<l[i]:
-                secondMax=max1
-                max1=l[i]
-            if l[i]>secondMax and l[i]!=max1:
-                secondMax=l[i]
-        return secondMax
+            if i.isdigit():
+                num=int(i)
+            if first==None:
+                first=num
+            elif num<first: second=max(second,num)
+            elif num>first:
+                temp=first
+                first=num
+                second=temp
+
+        return second
