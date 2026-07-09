1612f033c97b09ae088ba5e0f2d664669ae5d10f
nagendra1-ch <chnagendra592@gmail.com>
2026-07-09 15:11:23 +0000

    Sync LeetCode submission Runtime - 42 ms (80.46%), Memory - 19.3 MB (30.84%)

--- 0007-reverse-integer/solution.py
+++ 0007-reverse-integer/solution.py
@@ -5,7 +5,7 @@ class Solution:
             x=0-x
         reversed_x = int(str(x)[::-1]) * sign
         
-        # 32-bit signed integer range check
+        
         if reversed_x < -2**31 or reversed_x > 2**31 - 1:
             return 0
         return reversed_x
